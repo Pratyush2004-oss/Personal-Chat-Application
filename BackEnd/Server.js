d575597e27899fbce7438d2ef36b7af28361cb26
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-03-30 21:10:03 +0530

    Personal Chat Application

--- BackEnd/Server.js
+++ BackEnd/Server.js
@@ -1,3 +1,4 @@
+import path from "path"
 
 // Package Imports
 import express from 'express';
@@ -8,7 +9,7 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './Routes/auth.routes.js'
 import messageRoutes from './Routes/message.routes.js'
 import userRouters from './Routes/user.routes.js';
-import { app,io,server } from './Socket/Socket.js';       //importing Socket from Socket.js
+import { app,server } from './Socket/Socket.js';       //importing Socket from Socket.js
 
 // importing database connection 
 import connectToMongoDB from './DB/ConnectionMongoDB.js';
@@ -16,17 +17,18 @@ import connectToMongoDB from './DB/ConnectionMongoDB.js';
 // Variables
 const PORT = process.env.PORT || 3001;
 
+// Initializing the server and middleware
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+
+// used to render if the url is mismatched to the output in the Application
 
 // MiddleWares
 dotenv.config();
 app.use(express.json());// to parse incoming requests with JSON payloads (from req.body)
-app.use(cookieParser())
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
-
+app.use(cookieParser());
 
 // used for routing SignUp, Login pages for the user 
 app.use("/api/auth", authRoutes)
@@ -36,6 +38,16 @@ app.use('/api/messages', messageRoutes)
 
 // used to get users 
 app.use("/api/users", userRouters)
+
+app.get("*" , (req,res) => {
+
+    // setting the header 
+    res.setHeader('Content-Type', 'text/html');
+    // sending the file 
+    const filePath = path.join(__dirname,"frontend","dist","index.html")
+    res.sendFile(filePath)
+})
+
 server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server is running on port ${PORT}`)
